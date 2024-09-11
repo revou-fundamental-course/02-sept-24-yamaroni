67ce84a35c84863f6c632d93369ceb6fc468185c
Yama Roni <yamaroni8@gmail.com>
2024-09-11 20:02:29 +0700

    update js dan css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 // Ini Javascript
+
+function validateLuas() {
+    let panjangAlas = document.getElementById('panjang-alas').value;
+    let tinggi = document.getElementById('tinggi').value;
+
+    if (panjangAlas == '' || tinggi == '' || panjangAlas.includes('e') || tinggi.includes('e')) {
+        alert('Form Masih Kosong');
+    }
+    console.log(panjangAlas);
+    console.log(tinggi);
+}
